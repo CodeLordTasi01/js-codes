dff7b49e7c902e9e965630ca5a025d70f0426883
Tasleem Shah <tasleems021@gmail.com>
2025-08-23 10:42:49 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "tasleem@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "suyash@gmail.com"
+accountPassword = "121212"
+accountCity = "Bangalore"
+
+console.log(accountId); 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
