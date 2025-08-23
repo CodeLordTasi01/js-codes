003f661d8ba559ae199d400b47edd4ac1bd05416
Tasleem Shah <tasleems021@gmail.com>
2025-08-23 12:04:52 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,63 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+// let score = true
+// console.log(typeof score);
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+
+
+let score = "Tasleem"
+console.log(typeof score);
+console.log(score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// let valueInNumber = Number(score)
+// let stringNumber = String(number)
+// let booleanValue = Boolean(number)
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
