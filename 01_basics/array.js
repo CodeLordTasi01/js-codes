8f1f5376c7884413879ae354ce455ad280987274
CodeLordTasi01 <tasleems021@gmail.com>
2025-09-17 20:48:06 +0530

    Add array.js inside 01_basics folder

---
+++ 01_basics/array.js
@@ -0,0 +1,131 @@
+// const myArr = [1,2,3,4,5,6,7]
+// const myHeros = ["Thor","Hulk","Iron Man","Spider Man"]
+// const myArr2 = new Array(1,2,3,4,5,6)
+// console.log("Array is :",myArr2);
+// console.log("Array is :",myArr)
+// console.log("Array is :",myHeros)
+
+// myArr.push(8) // last mein element insert karne ke liye
+// console.log(myArr);
+
+// myArr.pop() // last se elment remove karne ke liye
+// console.log(myArr);
+
+// myArr.unshift(0) // starting mein element insert karne ke liye
+// console.log(myArr);
+
+// myArr.shift() // starting se element remove karne ke liye
+// console.log(myArr);
+
+// console.log(myArr.includes(2))
+// console.log(myArr.indexOf(2))
+
+
+// const newArr = myArr.join()
+// // console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+// const myArr1 = myArr.slice(1, 6)
+// console.log(myArr1)
+
+
+// const myArr2 = myArr.splice(1, 6)
+// console.log(myArr2)
+// console.log(myArr)
+
+
+
+// const Marvel_Heros = ['Thor','Iron man','Spider Man']
+// const Dc_Heros = ['Super Man', 'Bat Man','flash']
+// Marvel_Heros.push(Dc_Heros)
+// console.log(Marvel_Heros)
+// const Concat_Heros = Marvel_Heros.concat(Dc_Heros)
+// console.log(Concat_Heros)
+// const allHeros = [...Marvel_Heros, ...Dc_Heros]
+// console.log(allHeros)
+
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+
+// console.log(Array.isArray("Tasleem"))
+// console.log(Array.from("Tasleem"))
+// console.log(Array.from({name : 'tasleem'})) //Interesting views
+
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3= 300
+// console.log(Array.of(score1, score2, score3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function binarySearch(arr, target) {
+//     start = 0;
+//     end = arr.length - 1;
+//     while (start <= end) {
+//         mid = Math.floor(start + (end - start) / 2);
+//         if (arr[mid] === target) {
+//             return mid;  
+//         } else if (arr[mid] < target) {
+//             start = mid + 1;
+//         } else {
+//             end = mid - 1;
+//         }
+//     }
+//     return -1; 
+// }
+// arr = [2, 6, 8, 10, 50, 63, 90];
+// target = 8;
+// console.log(binarySearch(arr, target));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
